e05dd2d24e79f16901df781d13d4d673c401d4ca
Bronley Plumb <bronley@gmail.com>
2020-01-07 06:50:38 -0500

    add test to validate the roku-deploy@3.0.0-beta.6 fix is working.

--- src/ProgramBuilder.spec.ts
+++ src/ProgramBuilder.spec.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import * as path from 'path';
 import * as sinonImport from 'sinon';
 import { FileChangeType } from 'vscode-languageserver';
+import * as fsExtra from 'fs-extra';
 
 import { BsConfig } from './BsConfig';
 import { Program } from './Program';
@@ -80,6 +81,40 @@ describe('ProgramBuilder', () => {
                 await builder.run({});
                 expect(true).to.be.false('Should have thrown exception');
             } catch (e) { }
+        });
+
+        afterEach(() => {
+            try {
+                fsExtra.removeSync(`${rootDir}/testProject`);
+            } catch (e) {
+                console.error(e);
+            }
+        });
+
+        it.only('only adds the last file with the same pkg path', async () => {
+            //undo the vfs for this test
+            sinon.restore();
+            fsExtra.ensureDirSync(`${rootDir}/testProject/source`);
+            fsExtra.writeFileSync(`${rootDir}/testProject/source/lib1.brs`, 'sub doSomething()\nprint "lib1"\nend sub');
+            fsExtra.writeFileSync(`${rootDir}/testProject/source/lib2.brs`, 'sub doSomething()\nprint "lib2"\nend sub');
+
+            await builder.run({
+                rootDir: n(`${rootDir}/testProject`),
+                createPackage: false,
+                deploy: false,
+                copyToStaging: false,
+                //both files should want to be the `source/lib.brs` file...but only the last one should win
+                files: [{
+                    src: n(`${rootDir}/testProject/source/lib1.brs`),
+                    dest: 'source/lib.brs'
+                }, {
+                    src: n(`${rootDir}/testProject/source/lib2.brs`),
+                    dest: 'source/lib.brs'
+                }],
+            });
+            const diagnostics = builder.getDiagnostics();
+            expect(diagnostics.map(x => x.message)).to.eql([]);
+            expect(builder.program.getFileByPathAbsolute(n(``)))
         });
     });
 });
