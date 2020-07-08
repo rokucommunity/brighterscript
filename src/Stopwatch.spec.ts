28c0b9f45b01dd347e35d04b125bd8ae4424c632
Bronley Plumb <bronley@gmail.com>
2020-07-08 16:43:02 -0400

    Fix stopwatch spec

--- src/Stopwatch.spec.ts
+++ src/Stopwatch.spec.ts
@@ -28,10 +28,10 @@ describe('Stopwatch', () => {
     it('pretty prints', () => {
         let stopwatch = new Stopwatch();
         stopwatch.totalMilliseconds = 45;
-        expect(stopwatch.getDurationText()).to.equal('45ms');
+        expect(stopwatch.getDurationText()).to.equal('45.0ms');
         stopwatch.totalMilliseconds = 2000 + 45;
-        expect(stopwatch.getDurationText()).to.equal('2s45ms');
+        expect(stopwatch.getDurationText()).to.equal('2s45.0ms');
         stopwatch.totalMilliseconds = 180000 + 2000 + 45;
-        expect(stopwatch.getDurationText()).to.equal('3m2s45ms');
+        expect(stopwatch.getDurationText()).to.equal('3m2s45.0ms');
     });
 });
