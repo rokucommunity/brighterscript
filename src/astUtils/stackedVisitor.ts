c8d02af5a38c499d4f9f00c5697563a86a6ef7d4
Bronley Plumb <bronley@gmail.com>
2020-10-15 15:02:25 -0400

    fix formatting issues.

--- src/astUtils/stackedVisitor.ts
+++ src/astUtils/stackedVisitor.ts
@@ -2,7 +2,7 @@
  * Generic item/parent visitor providing a stack of the parents,
  * and callbacks when pushing/popping the stack.
  */
-export function createStackedVisitor<T> (
+export function createStackedVisitor<T>(
     visitor: (item: T, stack: T[]) => void,
     onPush?: (item: T, stack: T[]) => void,
     onPop?: (item: T, stack: T[]) => void
