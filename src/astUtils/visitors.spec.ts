04d7b72e7157a6d976cf7110d292674ce1f67958
Bronley Plumb <bronley@gmail.com>
2020-12-18 15:29:37 -0500

    fix broken tests

--- src/astUtils/visitors.spec.ts
+++ src/astUtils/visitors.spec.ts
@@ -295,6 +295,7 @@ describe('astUtils visitors', () => {
                 'ExpressionStatement:1:VariableExpression',       // exec("e", <some>())
                 'ForStatement:1:LiteralExpression',               // for i = <1> to 10
                 'AssignmentStatement:2:LiteralExpression',        // for <i = 1> to 10
+                'ForEachStatement:1:VariableExpression',          // for each <n> in aa
                 'ForEachStatement:1:VariableExpression',          // for each n in <aa>
                 'WhileStatement:1:BinaryExpression',              // while <i < 10>
                 'WhileStatement:1:VariableExpression',            // while <i> < 10
