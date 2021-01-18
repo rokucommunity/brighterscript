dfcc3b44f722fe9c3a308380bb8bcc44019c6b61
Bronley <bronley@gmail.com>
2021-01-18 06:35:31 -0500

    Fix docs compiler

--- scripts/compile-doc-examples.ts
+++ scripts/compile-doc-examples.ts
@@ -8,46 +8,46 @@ class DocCompiler {
     private docsFolder = path.resolve(path.join(__dirname, '..', 'docs'));
     public async run() {
 
-        var docs = glob.sync('**/*.md', {
+        let docs = glob.sync('**/*.md', {
             cwd: this.docsFolder,
             absolute: true
         });
 
         for (let docPath of docs) {
             console.log('\n', docPath);
-            await this.processDoc(docPath);
+            this.processDoc(docPath);
         }
     }
 
     private lines: string[];
     private index: number;
 
-    public async processDoc(docPath: string) {
+    public processDoc(docPath: string) {
         let contents = fsExtra.readFileSync(docPath).toString();
         this.lines = util.splitIntoLines(contents);
         this.index = 0;
         while (this.index < this.lines.length) {
             let line = this.lines[this.index];
             if (line.includes('```')) {
-                await this.processCodeBlock();
+                this.processCodeBlock();
             }
             this.index++;
         }
 
-        var result = this.lines.join('\n');
+        let result = this.lines.join('\n');
         fsExtra.writeFileSync(docPath, result);
         delete this.lines;
         this.index = -1;
     }
 
-    public async processCodeBlock() {
-        var sourceLines = [] as string[];
-        var sourceStartIndex = this.index + 1;
-        var sourceStopIndex: number;
+    public processCodeBlock() {
+        let sourceLines = [] as string[];
+        let sourceStartIndex = this.index + 1;
+        let sourceStopIndex: number;
 
         //find the rest of the source code block
         //+1 to step past the opening ```
-        for (var i = this.index + 1; i < this.lines.length; i++) {
+        for (let i = this.index + 1; i < this.lines.length; i++) {
             let line = this.lines[i];
             if (line.includes('```')) {
                 sourceStopIndex = i - 1;
@@ -59,11 +59,11 @@ class DocCompiler {
 
         let sourceCode = sourceLines.join('\n');
 
-        var transpiledStartIndex: number;
-        var transpiledStopIndex: number;
+        let transpiledStartIndex: number;
+        let transpiledStopIndex: number;
         //find the transpiled code block (there must be one after every
         //+2 to step past the last line of code, and the final ```
-        outer: for (var i = sourceStopIndex + 2; i < this.lines.length; i++) {
+        outer: for (let i = sourceStopIndex + 2; i < this.lines.length; i++) {
             let line = this.lines[i];
             //the next code block MUST be a brightscript block. hard-fail if it isn't
             if (line.includes('```')) {
@@ -88,9 +88,9 @@ class DocCompiler {
         //now that we have the range for the transpiled code, we need to transpile the source code
         if (transpiledStartIndex && transpiledStopIndex && sourceCode) {
             console.log(`Transpiling code block at lines ${sourceStartIndex}-${sourceStopIndex}`);
-            var transpiledCode = await this.transpile(sourceCode);
+            const transpiledCode = this.transpile(sourceCode);
             let transpiledLines = transpiledCode.split('\n');
-            let originalTranspiledLineCount = transpiledStopIndex - transpiledStartIndex
+            let originalTranspiledLineCount = transpiledStopIndex - transpiledStartIndex;
 
             //replace the old transpiled lines with the new ones
             this.lines.splice(
@@ -103,15 +103,15 @@ class DocCompiler {
         }
     }
 
-    public async transpile(code: string) {
-        var program = new Program({
+    public transpile(code: string) {
+        const program = new Program({
             rootDir: `${__dirname}/rootDir`,
             files: [
                 'source/main.brs'
             ]
         });
-        var file = await program.addOrReplaceFile({ src: `${__dirname}/rootDir/source/main.bs`, dest: 'source/main.bs' }, code)
-        await program.validate();
+        const file = program.addOrReplaceFile({ src: `${__dirname}/rootDir/source/main.bs`, dest: 'source/main.bs' }, code);
+        program.validate();
         let tranpileResult = file.transpile();
         return tranpileResult.code;
     }
