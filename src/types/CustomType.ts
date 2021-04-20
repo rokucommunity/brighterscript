13639b52e23d5fa1683b9b32a1eeadea9d0b6ee6
Mark Pearce <mark.pearce@redspace.com>
2021-04-20 10:48:27 -0300

    Makes CustomType display its actual type

--- src/types/CustomType.ts
+++ src/types/CustomType.ts
@@ -6,4 +6,12 @@ export class CustomType extends ObjectType implements BscType {
     constructor(public name: string) {
         super();
     }
+
+    public toString(): string {
+        return this.name;
+    }
+
+    public toTypeString(): string {
+        return 'object';
+    }
 }
