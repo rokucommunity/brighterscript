74aed13bd8ae911b535bae3923553d684ba592cc
Bronley <bronley@gmail.com>
2021-07-14 06:19:29 -0400

    fix bugs finding interface implementors. generate ts file instead of json

--- .eslintrc.js
+++ .eslintrc.js
@@ -209,5 +209,11 @@ module.exports = {
             '@typescript-eslint/no-require-imports': 'off',
             '@typescript-eslint/no-var-requires': 'off'
         }
+    }, {
+        files: ['src/roku-types/data.ts'],
+        rules: {
+            '@typescript-eslint/quotes': 'off',
+            'no-template-curly-in-string': 'off'
+        }
     }]
 };
