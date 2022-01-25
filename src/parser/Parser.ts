83ec013760e9ec1e33d47e7fdd8485f75660b690
Bronley Plumb <bronley@gmail.com>
2022-01-25 12:55:13 -0500

    Fix bug breaking parser after interface statement.

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -480,8 +480,6 @@ export class Parser {
         //consume the final `end interface` token
         const endInterfaceToken = this.consumeToken(TokenKind.EndInterface);
 
-        this.consumeStatementSeparators();
-
         const statement = new InterfaceStatement(
             interfaceToken,
             nameToken,
