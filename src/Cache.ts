ed55d8052f0a1369d8b528f5e0658ba0c0732658
Bronley Plumb <bronley@gmail.com>
2022-02-03 06:04:41 -0500

    Add generics support to Cache class. Use map.

--- src/Cache.ts
+++ src/Cache.ts
@@ -1,23 +1,26 @@
 /**
  * Synchronous cache
  */
-export class Cache {
-    private cache = {} as Record<string, any>;
+export class Cache<TKey = any, TValue = any> {
+    private cache = new Map<TKey, TValue>();
 
     /**
      * Get value from the cache if it exists,
      * otherwise call the factory function to create the value, add it to the cache, and return it.
      */
-    public getOrAdd<T>(key: string, factory: (key: string) => T) {
-        if (this.cache[key] === undefined) {
-            this.cache[key] = factory(key);
+    public getOrAdd<K extends TKey, T extends TValue>(key: K, factory: (key: K) => T): T {
+        if (!this.cache.has(key)) {
+            const value = factory(key);
+            this.cache.set(key, value);
+            return value;
+        } else {
+            return this.cache.get(key) as T;
         }
-        return this.cache[key] as T;
     }
     /**
      * Clear the cache
      */
     public clear() {
-        this.cache = {};
+        this.cache.clear();
     }
 }
