f17b5fe98ec228f1eb29d12b9eb93716598ee9b1
Bronley Plumb <bronley@gmail.com>
2022-02-09 10:42:14 -0500

    Cache extens map.

--- src/Cache.ts
+++ src/Cache.ts
@@ -1,26 +1,19 @@
 /**
  * A cache that will call the factory to create the item if it doesn't exist
  */
-export class Cache<TKey = any, TValue = any> {
-    private cache = new Map<TKey, TValue>();
+export class Cache<TKey = any, TValue = any> extends Map<TKey, TValue> {
 
     /**
      * Get value from the cache if it exists,
      * otherwise call the factory function to create the value, add it to the cache, and return it.
      */
-    public getOrAdd<K extends TKey, T extends TValue>(key: K, factory: (key: K) => T): T {
-        if (!this.cache.has(key)) {
+    public getOrAdd(key: TKey, factory: (key: TKey) => TValue): TValue {
+        if (!this.has(key)) {
             const value = factory(key);
-            this.cache.set(key, value);
+            this.set(key, value);
             return value;
         } else {
-            return this.cache.get(key) as T;
+            return this.get(key);
         }
-    }
-    /**
-     * Clear the cache
-     */
-    public clear() {
-        this.cache.clear();
     }
 }
