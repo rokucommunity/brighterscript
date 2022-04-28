1327541f3dcd7da1e936ea087f693718edadabe0
Bronley Plumb <bronley@gmail.com>
2022-04-28 14:54:08 -0400

    More flexible cache typing

--- src/Cache.ts
+++ src/Cache.ts
@@ -7,13 +7,13 @@ export class Cache<TKey = any, TValue = any> extends Map<TKey, TValue> {
      * Get value from the cache if it exists,
      * otherwise call the factory function to create the value, add it to the cache, and return it.
      */
-    public getOrAdd(key: TKey, factory: (key: TKey) => TValue): TValue {
+    public getOrAdd<R extends TValue = TValue>(key: TKey, factory: (key: TKey) => R): R {
         if (!this.has(key)) {
             const value = factory(key);
             this.set(key, value);
             return value;
         } else {
-            return this.get(key);
+            return this.get(key) as R;
         }
     }
 }
