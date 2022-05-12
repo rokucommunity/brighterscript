4297c55735ff61c52cbc19bbc39feee589723add
Bronley Plumb <bronley@gmail.com>
2022-05-12 08:35:43 -0400

    Fix semantic tokens for enums in if statements

--- src/bscPlugin/semanticTokens/BrsFileSemanticTokensProcessor.spec.ts
+++ src/bscPlugin/semanticTokens/BrsFileSemanticTokensProcessor.spec.ts
@@ -76,4 +76,68 @@ describe('BrsFileSemanticTokensProcessor', () => {
             tokenType: SemanticTokenTypes.enumMember
         }]);
     });
+
+    it('matches each namespace section for enum', () => {
+        const file = program.setFile<BrsFile>('source/main.bs', `
+            sub main()
+                print Sentients.Humanoids.HumanoidType.Cylon
+            end sub
+            namespace Sentients.Humanoids
+                enum HumanoidType
+                    Human
+                    Alien
+                    Cylon
+                end enum
+            end namespace
+        `);
+        program.validate();
+        expectZeroDiagnostics(program);
+        expect(
+            util.sortByRange(program.getSemanticTokens(file.srcPath))
+        ).to.eql([{
+            range: util.createRange(2, 22, 2, 31),
+            tokenType: SemanticTokenTypes.namespace
+        }, {
+            range: util.createRange(2, 32, 2, 41),
+            tokenType: SemanticTokenTypes.namespace
+        }, {
+            range: util.createRange(2, 42, 2, 54),
+            tokenType: SemanticTokenTypes.enum
+        }, {
+            range: util.createRange(2, 55, 2, 60),
+            tokenType: SemanticTokenTypes.enumMember
+        }]);
+    });
+
+    it('matches enums in if statements', () => {
+        const file = program.setFile<BrsFile>('source/main.bs', `
+            sub main()
+                if Humanoids.HumanoidType.Cylon = "Cylon" then
+                    print true
+                end if
+            end sub
+            namespace Humanoids
+                enum HumanoidType
+                    Human = "Human"
+                    Alien = "Alien"
+                    Cylon = "Cylon"
+                end enum
+            end namespace
+        `);
+        program.validate();
+        expectZeroDiagnostics(program);
+        expect(
+            util.sortByRange(program.getSemanticTokens(file.srcPath))
+        ).to.eql([{
+            range: util.createRange(2, 19, 2, 28),
+            tokenType: SemanticTokenTypes.namespace
+        }, {
+            range: util.createRange(2, 29, 2, 41),
+            tokenType: SemanticTokenTypes.enum
+        }, {
+            range: util.createRange(2, 42, 2, 47),
+            tokenType: SemanticTokenTypes.enumMember
+        }]);
+    });
+
 });
