eff485fe08685d47f6c245b220031b3ebdaddcc8
Mark Pearce <mark.pearce@redspace.com>
2023-07-27 09:22:20 -0300

    revert change

--- src/roku-types/index.ts
+++ src/roku-types/index.ts
@@ -3,7 +3,7 @@ import data = require('./data.json');
 
 //apply any transforms/overrides before exporting
 
-export const nodes = data.nodes || {};
+export const nodes = data.nodes;
 export const components = data.components;
 export const interfaces = data.interfaces;
 export const events = data.events;
