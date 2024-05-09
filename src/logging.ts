de74ddbdfdb1058cff3aa66ff040608a5049febc
Bronley Plumb <bronley@gmail.com>
2024-05-09 16:08:11 -0400

    Fix formatting with logger output

--- src/logging.ts
+++ src/logging.ts
@@ -1,9 +1,14 @@
-import { default as logger, createLogger } from '@rokucommunity/logger';
+import { default as defaultLogger } from '@rokucommunity/logger';
 import type { Logger } from '@rokucommunity/logger';
+const logger = defaultLogger.createLogger();
 
 //force log levels to be same width
 logger.consistentLogLevelWidth = true;
+logger.printLogLevel = false;
+logger.timestampFormat = 'hh:mm:ss:SSS aa';
 logger.logLevel = 'log';
+logger.prefix = '';
 
-export { logger, Logger, createLogger };
+export const createLogger = logger.createLogger.bind(logger);
+export { logger, Logger };
 export { LogLevel } from './Logger';
