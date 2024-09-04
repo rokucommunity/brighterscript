091eeb31becf7fb26de850e0f161a7fbda14067e
Mark Pearce <mark.pearce@redspace.com>
2024-09-04 18:48:03 -0300

    Added a BrsDoc Parser

---
+++ src/parser/BrightScriptDocParser.spec.ts
@@ -0,0 +1,138 @@
+import { expect } from 'chai';
+import { BrightScriptDocParser } from './BrightScriptDocParser';
+
+describe('BrightScriptDocParser', () => {
+
+    const parser = new BrightScriptDocParser();
+
+    it('should get a comment', () => {
+        const doc = parser.parse('this is a comment');
+        expect(doc.description).to.equal('this is a comment');
+    });
+
+    it('should get a tag', () => {
+        const doc = parser.parse(`
+            this is a comment
+            @sometag here is the rest
+        `);
+        expect(doc.description).to.equal('this is a comment');
+        expect(doc.tags.length).to.equal(1);
+        expect(doc.tags[0].tagName).to.equal('sometag');
+        expect(doc.tags[0].detail).to.equal('here is the rest');
+        expect(doc.getTag('sometag').detail).to.equal('here is the rest');
+    });
+
+    it('should get a multiline comment', () => {
+        const doc = parser.parse(`
+            this is a comment
+            this is some more of a comment
+        `);
+        expect(doc.description).to.equal('this is a comment\nthis is some more of a comment');
+    });
+
+    describe('parseParam', () => {
+
+        it('should find @param tags of various types', () => {
+            const doc = parser.parse(`
+                this is a comment
+                @param p1
+                @param p2 description of p2
+                @param {some.type} p3
+                @param {some.type} p4 description of p4
+                @param [p5] optional p5
+                @param {some.type} [p6] optional with type p6
+                @param p7 multi line description
+                       of p7
+                @param p8
+                        description of p8
+            `);
+
+            expect(doc.getAllTags('param').length).to.equal(8);
+
+            expect(doc.getParam('p1').description).to.equal('');
+            expect(doc.getParam('p1').type).to.equal('');
+
+            expect(doc.getParam('p2').description).to.equal('description of p2');
+            expect(doc.getParam('p2').type).to.equal('');
+
+            expect(doc.getParam('p3').description).to.equal('');
+            expect(doc.getParam('p3').type).to.equal('some.type');
+
+            expect(doc.getParam('p4').description).to.equal('description of p4');
+            expect(doc.getParam('p4').type).to.equal('some.type');
+
+            expect(doc.getParam('p5').description).to.equal('optional p5');
+            expect(doc.getParam('p5').type).to.be.equal('');
+            expect(doc.getParam('p5').optional).to.be.true;
+
+            expect(doc.getParam('p6').description).to.equal('optional with type p6');
+            expect(doc.getParam('p6').type).to.be.equal('some.type');
+            expect(doc.getParam('p6').optional).to.be.true;
+
+            expect(doc.getParam('p7').description).to.equal('multi line description\nof p7');
+            expect(doc.getParam('p7').type).to.be.equal('');
+            expect(doc.getParam('p7').optional).to.be.false;
+
+            expect(doc.getParam('p8').description).to.equal('description of p8');
+            expect(doc.getParam('p8').type).to.equal('');
+        });
+    });
+
+    it('includes the @description tag in the description', () => {
+        const doc = parser.parse(`
+            this is a comment
+            @description this is a description
+        `);
+        expect(doc.description).to.equal('this is a comment\nthis is a description');
+    });
+
+    it('includes the @description tag in the description when multiline', () => {
+        const doc = parser.parse(`
+            this is a comment
+
+            above space intentionally blank
+            @description this is a description
+
+             above space intentionally blank again
+            @param whatever
+             this will be the description of whatever
+
+        `);
+        expect(doc.description).to.equal('this is a comment\n\nabove space intentionally blank\nthis is a description\n\nabove space intentionally blank again');
+    });
+
+    it('includes the @return tag', () => {
+        const doc = parser.parse(`
+            this is a comment
+            @return this is a return
+        `);
+        expect(doc.getReturn().description).to.equal('this is a return');
+    });
+
+    it('includes the @return tag when it has a type', () => {
+        const doc = parser.parse(`
+            this is a comment
+            @return {some.thing.here} this is a return
+        `);
+        expect(doc.getReturn().description).to.equal('this is a return');
+        expect(doc.getReturn().type).to.equal('some.thing.here');
+    });
+
+    it('allows the @returns (with an s)', () => {
+        const doc = parser.parse(`
+            this is a comment
+            @returns {some.thing.here} this is a returns
+        `);
+        expect(doc.getReturn().description).to.equal('this is a returns');
+        expect(doc.getReturn().type).to.equal('some.thing.here');
+    });
+
+
+    it('finds the type tag', () => {
+        const doc = parser.parse(`
+            @type {integer}
+        `);
+        expect(doc.getTypeTag().type).to.equal('integer');
+    });
+
+});
---
+++ src/parser/BrightScriptDocParser.ts
@@ -0,0 +1,220 @@
+
+
+const tagRegex = /@(\w+)(?:\s+(.*))?/;
+const paramRegex = /(?:{([^}]*)}\s+)?(?:(\[?\w+\]?))\s*(.*)/;
+const returnRegex = /(?:{([^}]*)})?\s*(.*)/;
+const typeTagRegex = /(?:{([^}]*)})?/;
+
+export class BrightScriptDocParser {
+
+    public parse(documentation: string) {
+        const lines = documentation.split('\n');
+        const brsDoc = new BrightScriptDoc(documentation);
+        const blockLines = [] as string[];
+        const descriptionLines = [] as string[];
+        let lastTag: BrsDocTag;
+        function augmentLastTagWithBlockLines() {
+            if (blockLines.length > 0 && lastTag) {
+                // add to the description or details to previous tag
+                if (typeof (lastTag as BrsDocWithDescription).description !== 'undefined') {
+                    (lastTag as BrsDocWithDescription).description += '\n' + blockLines.join('\n');
+                    (lastTag as BrsDocWithDescription).description = (lastTag as BrsDocWithDescription).description.trim();
+                }
+                if (typeof lastTag.detail !== 'undefined') {
+                    lastTag.detail += '\n' + blockLines.join('\n');
+                    lastTag.detail = lastTag.detail.trim();
+                }
+            }
+            blockLines.length = 0;
+        }
+        for (let line of lines) {
+            line = line.trim();
+            if (!line.startsWith('@')) {
+                if (lastTag) {
+
+                    blockLines.push(line);
+                } else if (descriptionLines.length > 0 || line) {
+                    // add a line to the list if it's not empty
+                    descriptionLines.push(line);
+                }
+            } else {
+                augmentLastTagWithBlockLines();
+                const newTag = this.parseLine(line);
+                lastTag = newTag;
+                if (newTag) {
+                    brsDoc.tags.push(newTag);
+                }
+            }
+        }
+        augmentLastTagWithBlockLines();
+        brsDoc.description = descriptionLines.join('\n').trim();
+        return brsDoc;
+    }
+
+    private parseLine(line: string) {
+        line = line.trim();
+        const match = tagRegex.exec(line);
+        if (!match) {
+            return;
+        }
+        const tagName = match[1].toLowerCase();
+        const detail = match[2] ?? '';
+
+        switch (tagName) {
+            case 'param':
+                return this.parseParam(detail);
+            case 'return':
+            case 'returns':
+                return this.parseReturn(detail);
+            case 'type':
+                return this.parseType(detail);
+        }
+        return {
+            tagName: tagName,
+            detail: detail
+        };
+    }
+
+
+    private parseParam(detail: string): BrsDocParamTag {
+        let type = '';
+        let description = '';
+        let optional = false;
+        let paramName = '';
+        let match = paramRegex.exec(detail);
+        if (match) {
+            type = match[1] ?? '';
+            paramName = match[2] ?? '';
+            description = match[3] ?? '';
+        } else {
+            paramName = detail.trim();
+        }
+        if (paramName) {
+            optional = paramName.startsWith('[') && paramName.endsWith(']');
+            paramName = paramName.replace(/\[|\]/g, '').trim();
+        }
+        return {
+            tagName: 'param',
+            name: paramName,
+            type: type,
+            description: description,
+            optional: optional,
+            detail: detail
+        };
+    }
+
+    private parseReturn(detail: string): BrsDocWithDescription {
+        let match = returnRegex.exec(detail);
+        let type = '';
+        let description = '';
+        if (match) {
+            if (match[2]) {
+                type = match[1] ?? '';
+                description = match[2] ?? '';
+            } else if (match[1]) {
+                description = match[1] ?? '';
+            }
+        }
+        return {
+            tagName: 'return',
+            type: type,
+            description: description,
+            detail: detail
+        };
+    }
+
+    private parseType(detail: string): BrsDocWithType {
+        let match = typeTagRegex.exec(detail);
+        let type = '';
+        if (match) {
+            if (match[1]) {
+                type = match[1] ?? '';
+            }
+        }
+        return {
+            tagName: 'type',
+            type: type,
+            detail: detail
+        };
+    }
+}
+
+class BrightScriptDoc {
+
+    protected _description: string;
+
+    public tags = [] as BrsDocTag[];
+
+    constructor(
+        public readonly documentation: string
+    ) {
+    }
+
+    set description(value: string) {
+        this._description = value;
+    }
+
+    get description() {
+        const descTag = this.tags.find((tag) => {
+            return tag.tagName === 'description';
+        });
+
+        let result = this._description ?? '';
+        if (descTag) {
+            const descTagDetail = descTag.detail;
+            result = result ? result + '\n' + descTagDetail : descTagDetail;
+        }
+        return result.trim();
+    }
+
+    getParam(name: string) {
+        return this.tags.find((tag) => {
+            return tag.tagName === 'param' && (tag as BrsDocParamTag).name === name;
+        }) as BrsDocParamTag;
+    }
+
+    getReturn() {
+        return this.tags.find((tag) => {
+            return tag.tagName === 'return' || tag.tagName === 'returns';
+        }) as BrsDocWithDescription;
+    }
+
+    getTypeTag() {
+        return this.tags.find((tag) => {
+            return tag.tagName === 'type';
+        }) as BrsDocWithType;
+    }
+
+    getTag(tagName: string) {
+        const lowerTagName = tagName.toLowerCase();
+        return this.tags.find((tag) => {
+            return tag.tagName === lowerTagName;
+        });
+    }
+
+    getAllTags(tagName: string) {
+        const lowerTagName = tagName.toLowerCase();
+        return this.tags.filter((tag) => {
+            return tag.tagName === lowerTagName;
+        });
+    }
+
+
+}
+
+interface BrsDocTag {
+    tagName: string;
+    detail?: string;
+}
+interface BrsDocWithType extends BrsDocTag {
+    type?: string;
+}
+
+interface BrsDocWithDescription extends BrsDocWithType {
+    description?: string;
+}
+
+interface BrsDocParamTag extends BrsDocWithDescription {
+    name: string;
+    optional?: boolean;
+}
