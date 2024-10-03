3016c3561c8c90614dee9d868631bf5a5a1fea36
Bronley Plumb <bronley@gmail.com>
2024-10-03 12:22:31 -0400

    Fix bug in bslib prefix transpiling

--- src/bscPlugin/serialize/BslibInjector.spec.ts
+++ src/bscPlugin/serialize/BslibInjector.spec.ts
@@ -2,18 +2,48 @@ import { createSandbox } from 'sinon';
 import * as fsExtra from 'fs-extra';
 import { Program } from '../../Program';
 import { tempDir, rootDir } from '../../testHelpers.spec';
+import { BslibManager } from './BslibManager';
+import { expect } from 'chai';
 const sinon = createSandbox();
 
 describe('BslibInjector', () => {
 
     let program: Program;
+    let manager: BslibManager;
 
     beforeEach(() => {
         fsExtra.emptyDirSync(tempDir);
         program = new Program({ rootDir: rootDir, sourceMap: true });
+        manager = new BslibManager();
     });
     afterEach(() => {
         sinon.restore();
         program.dispose();
+    });
+
+    describe('isBslibPkgPath', () => {
+        it('works for valid paths', () => {
+            expect(
+                BslibManager.isBslibPkgPath('source/bslib.brs')
+            ).to.be.true;
+            expect(
+                BslibManager.isBslibPkgPath('source/roku_modules/bslib/bslib.brs')
+            ).to.be.true;
+            expect(
+                BslibManager.isBslibPkgPath('source/roku_modules/rokucommunity_bslib/bslib.brs')
+            ).to.be.true;
+        });
+
+        it('works for invalid paths', () => {
+            expect(
+                BslibManager.isBslibPkgPath('source/bslib2.brs')
+            ).to.be.false;
+            expect(
+                BslibManager.isBslibPkgPath('source/roku_modules/1bslib/bslib.brs')
+            ).to.be.false;
+            expect(
+                BslibManager.isBslibPkgPath('source/roku_modules/rokucommunity_bslib/3bslib.brs')
+            ).to.be.false;
+        });
     });
 });
