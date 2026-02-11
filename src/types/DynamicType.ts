4a5597851a89928c46f6949744aafda5386038d3
Mark Pearce <mark.pearce@redspace.com>
2026-02-11 07:43:23 -0400

    remove typestatement look up in dynamic isequal

--- src/types/DynamicType.ts
+++ src/types/DynamicType.ts
@@ -1,5 +1,5 @@
 import type { TypeCompatibilityData } from '../interfaces';
-import { isDynamicType, isTypeStatementType, isUninitializedType, isVoidType } from '../astUtils/reflection';
+import { isDynamicType, isUninitializedType, isVoidType } from '../astUtils/reflection';
 import { BscType } from './BscType';
 import { BscTypeKind } from './BscTypeKind';
 import { BuiltInInterfaceAdder } from './BuiltInInterfaceAdder';
@@ -42,9 +42,6 @@ export class DynamicType extends BscType {
     }
 
     public isEqual(targetType: BscType) {
-        while (isTypeStatementType(targetType)) {
-            targetType = targetType.wrappedType;
-        }
         return isDynamicType(targetType);
     }
 
