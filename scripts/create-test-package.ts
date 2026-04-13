670d253d971d14936e46f4b23faa8ad8dc998eef
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-04-13 16:17:07 +0200

    perf: migrate to tinyclip from clipboardy

--- scripts/create-test-package.ts
+++ scripts/create-test-package.ts
@@ -1,7 +1,7 @@
 import { execSync } from 'child_process';
 import * as fsExtra from 'fs-extra';
-import { standardizePath as s } from '../src';
-import * as clipboard from 'clipboardy';
+import { standardizePath as s } from '../src/util';
+import * as clipboard from 'tinyclip';
 
 /**
  * Create a temporary package with a unique name to prevent local caching
@@ -21,7 +21,7 @@ try {
     execSync('npm run build && npm pack', { stdio: 'inherit', cwd: `${__dirname}/../` });
     fsExtra.moveSync(`brighterscript-${version}-test.${date}.tgz`, newPath);
     console.log(newPath);
-    clipboard.writeSync('npm i file:' + newPath);
+    clipboard.writeText('npm i file:' + newPath);
     console.log('copied to clipboard');
 } finally {
     //restore the version rename the version
